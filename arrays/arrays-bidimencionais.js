996c8ccbd91cd1718b1335fdbd9bd5203792d081
Douglas de Souza Luz <dougs.2210@gmail.com>
2020-07-25 22:01:50 -0300

    metodo toString para transformar arrays em string

--- arrays/arrays-bidimencionais.js
+++ arrays/arrays-bidimencionais.js
@@ -278,3 +278,6 @@ console.log(numbers.includes(20))
 let numbers2 = [7, 6, 5, 4, 3, 2, 1]
 console.log(numbers2.includes(4, 5))
 
+/** Convertendo array em string */
+
+console.log(numbers.toString())
